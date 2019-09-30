12dc98e1e071945984ff426932a8d77aa59288c7
thuthuatna <thuthuatna@gmail.com>
2019-10-01 00:55:39 +0700

    v1

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+
+  navigateToLogin(){
+    this.router.navigate(['login']);
+  }
 
 }
